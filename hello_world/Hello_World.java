a02604383fc76275c653c7492716dfb898a44acd
Pankaj Agrawal <erpankaj182@gmail.com>
2017-10-27 12:48:01 +0530

    Create Hello_World.java

---
+++ hello_world/Hello_World.java
@@ -0,0 +1,11 @@
+AUTHOR: Pankaj Agrawal
+LANGUAGE: Java
+GITHUB: https://github.com/erpankaj182
+
+class Hello_World
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+	}
+}
